59e50cef9cfe363029552862eca1d6bfa55d6aa7
samsbg <70488844+samsbg@users.noreply.github.com>
2024-02-23 12:02:10 -0600

    Selection list

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,11 +9,11 @@ import { HeroesComponent } from './heroes/heroes.component';
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    HeroesComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
